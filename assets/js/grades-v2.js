e15c98e70fd5abe388711c3adf6624caff1960bb
Wilizilla <wilian.ventura@gmail.com>
2024-08-11 03:38:07 -0300

    Addition of communication between javascript and html, calculations and layout adjustments

--- assets/js/grades-v2.js
+++ assets/js/grades-v2.js
@@ -5,12 +5,29 @@ formulario.addEventListener('submit', function(event) {
 
     event.preventDefault(); // evita que o formulário recarregue
 
-    const n1 = document.getElementById('form-n1').value;
-    const ap = document.getElementById('form-ap').value;
-    const ai = document.getElementById('form-ai').value;
+    var n1String = document.getElementById('form-n1').value;
+    var apString = document.getElementById('form-ap').value;
+    var aiString = document.getElementById('form-ai').value;
 
-    console.log('nota em N1:', n1);
-    console.log('nota em AP:', ap);
-    console.log('nota em AI:', ai);
+    var n1 = Number(n1String);
+    var ap = Number(apString);
+    var ai = Number(aiString);
+
+    if (n1 == '') {
+        console.log('Nenhuma nota lançada');
+        document.getElementById("p-n1").innerHTML = `<p>Não há notas disponíveis</p>`;
+
+    } else {
+        console.log('nota em N1:', n1);
+        console.log('nota em AP:', ap);
+        console.log('nota em AI:', ai);
+
+        let n2= (ap+ai)/2
+
+        document.getElementById("p-n1").innerHTML = `<p>${n1}</p>`;
+        document.getElementById("p-n2").innerHTML = `<p>${n2}</p>`;
+        document.getElementById("p-n3").innerHTML = `<p>${nome}</p>`;
+
+    }
 
 });
