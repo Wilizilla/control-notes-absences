74cce9165d775e0f2484bdc8bef8376f7ddf6934
Wilizilla <wilian.ventura@gmail.com>
2024-08-15 17:22:43 -0300

    ux and ui increments in forms

--- assets/js/decimal-control.js
+++ assets/js/decimal-control.js
@@ -1,7 +1,7 @@
 //Essa parte não foi idéia minha, peguei exemplos da internet e chat gpt
 
 function inputAjust(inputElement) {
-    inputElement.addEventListener('change', function (e) {
+    inputElement.addEventListener('input', function (e) {
         let value = e.target.value;
 
         // Remove qualquer caractere que não seja número
@@ -21,21 +21,10 @@ function inputAjust(inputElement) {
         // Converte para número decimal
         let numericValue = parseFloat(value) / 100;
 
-        // Limita o valor a 10.00 - aqui
-      /*   if (numericValue > 10.00) {
-            numericValue = 10.00;
-        } */
-
         // Atualiza o valor do campo de texto com a máscara aplicada
         e.target.value = numericValue.toFixed(2);
     });
 }
-
-// exemplo usando get byid.
-
-/* const inputField = document.getElementById('form-n1');
-inputAjust(inputField);
- */
 
 document.querySelectorAll('.simple-input').forEach(function(inputElement) {
     inputAjust(inputElement)
--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,21 +1,38 @@
-document.getElementById('form-grades').addEventListener('submit', function(event){
+document.getElementById('form-grades').addEventListener('input', function(event){
    
-    const validationN1 = document.getElementById('form-n1').value;
-    const validationAp = document.getElementById('form-ap').value;
-    const validationAi = document.getElementById('form-ai').value;
-    const validationN3 = document.getElementById('form-n3').value;
+    const styleN1 = document.querySelector('#form-n1');
+    const styleAp = document.querySelector('#form-ap');
+    const styleAi = document.querySelector('#form-ai');
+    const styleN3 = document.querySelector('#form-n3');
+    
+    let validationN1 = styleN1.value;
+    let validationAp = styleAp.value;
+    let validationAi = styleAi.value;
+    let validationN3 = styleN3.value;
 
-    // validação N1
+    
+    /* const validationN1 = document.querySelector('#form-n1').value;
+    const validationAp = document.querySelector('#form-ap').value;
+    const validationAi = document.querySelector('#form-ai').value;
+    const validationN3 = document.querySelector('#form-n3').value; */
+    
+    // event.preventDefault(); // Impede o envio do formulário se houver campos inválidos
+
+    // validação N1 ###################################
     if (validationN1 > 10){
-        document.getElementById('n1-error').innerHTML = 'O valor da nota não pode ser maior que 10.'
+        document.getElementById('n1-error').innerHTML = 'O valor da nota não pode ser maior que 10.';
+        styleN1.classList.add('simple-input-invalid');
     }
     else{
         document.getElementById('n1-error').innerHTML = ''
+        styleN1.classList.remove('simple-input-invalid');
     }
 
     // validação AP
     if (validationAp > 10){
-        document.getElementById('ap-error').innerHTML = 'O valor da nota não pode ser maior que 10.'
+        document.getElementById('ap-error').innerHTML = 'O valor da nota não pode ser maior que 10.';
+        styleAp.classList.remove('simple-input');
+        styleAp.classList.add('simple-input-invalid');
     }
     else{
         document.getElementById('ap-error').innerHTML = ''
@@ -23,7 +40,9 @@ document.getElementById('form-grades').addEventListener('submit', function(event
 
     // validação AI
     if (validationAi > 10){
-        document.getElementById('ai-error').innerHTML = 'O valor da nota não pode ser maior que 10.'
+        document.getElementById('ai-error').innerHTML = 'O valor da nota não pode ser maior que 10.';
+        styleAi.classList.remove('simple-input');
+        styleAi.classList.add('simple-input-invalid');
     }
     else{
         document.getElementById('ai-error').innerHTML = ''
@@ -31,10 +50,13 @@ document.getElementById('form-grades').addEventListener('submit', function(event
 
     // validação N3
     if (validationN3 > 10){
-        document.getElementById('n3-error').innerHTML = 'O valor da nota não pode ser maior que 10.'
+        document.getElementById('n3-error').innerHTML = 'O valor da nota não pode ser maior que 10.';
+        styleN3.classList.remove('simple-input');
+        styleN3.classList.add('simple-input-invalid');
     }
     else{
         document.getElementById('n3-error').innerHTML = ''
     }
 
 });
+
